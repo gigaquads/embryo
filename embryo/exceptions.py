50514723c9681d4c3f658e597cfc8451d87b44d6
Daniel Gabriele <d.gabri3le@gmail.com>
2017-09-07 15:31:45 -0400

    Started support for pre and post_create hooks in embryos

---
+++ embryo/exceptions.py
@@ -0,0 +1,12 @@
+class EmbryoError(Exception):
+    pass
+
+
+class TemplateNotFound(EmbryoError):
+    def __init__(self, template_name):
+        message = (
+            '"{}" template not found. make sure you have '
+            'a templates directory in your embryo directory, containing '
+            'a template file with the expected name.'.format(template_name)
+            )
+        super().__init__(message)

4f67b50b3592bb38d2a22a445197f84e57fe266e
Daniel Gabriele <d.gabri3le@gmail.com>
2017-09-19 15:24:31 -0400

    Improved embryo search path logic

--- embryo/create.py
+++ embryo/create.py
@@ -7,6 +7,8 @@ import argparse
 
 import yaml
 
+from pprint import pprint
+
 from jinja2 import Template
 
 from embryo import Project
@@ -17,14 +19,14 @@ from .environment import build_env
 
 class EmbryoGenerator(object):
     def __init__(self):
+        self.env = build_env()
         self.here = os.path.dirname(os.path.realpath(__file__))
         self.embryos_dir = '{}/embryos'.format(self.here)
         self.embryo_path = None
-        self.embryo_search_path = [
-            os.getcwd(),
-            self.embryos_dir,
-        ]
-        self.env = build_env()
+        self.embryo_search_path = [os.getcwd()]
+        if 'EMBRYO_PATH' in os.environ:
+            path = os.environ['EMBRYO_PATH']
+            self.embryo_search_path.extend(path.split(':'))
 
     def create(self, args):
         self.args = args
@@ -32,10 +34,15 @@ class EmbryoGenerator(object):
             self.embryo_path = args.embryo
         else:
             for path in self.embryo_search_path:
-                embryo_path = '{}/{}'.format(path, args.embryo)
+                embryo_path = '{}/{}'.format(path.rstrip('/'), args.embryo)
                 if os.path.exists(embryo_path):
                     self.embryo_path = embryo_path
                     break
+
+        # ensure the embryo_path is absolute
+        self.embryo_path = os.path.realpath(self.embryo_path)
+
+        sys.path.append(self.embryo_path)
 
         context = self.load_context(args)
         hooks = self.load_hooks()
@@ -48,6 +55,10 @@ class EmbryoGenerator(object):
         templates = self.load_templates(args.embryo, context)
 
         print('>>> Creating embryo...')
+        print('-' * 80)
+        print('>>> Context:')
+
+        pprint(context, indent=2)
 
         root = args.destination
         project = Project(root=root, tree=tree, templates=templates)
@@ -57,15 +68,6 @@ class EmbryoGenerator(object):
         if hooks.post_create:
             print('>>> Running post_create hook...')
             hooks.post_create(project, context)
-
-    def embryo_names(self):
-        """
-        A list of all embryos by name
-        """
-        return [
-            x for x in os.listdir(self.embryos_dir)
-            if os.path.isdir(self.embryos_dir + '/' + x)
-        ]
 
     def load_templates(self, embryo: str, context: dict=None):
         templates_dir = os.path.join(self.embryo_path, 'templates')
@@ -130,21 +132,12 @@ class EmbryoGenerator(object):
         return context
 
     def load_hooks(self):
-        if self.embryo_path.startswith('/'):
-            abs_dirpath = self.embryo_path
-        else:
-            abs_dirpath = os.path.realpath(self.embryo_path)
-
-        abs_filepath = os.path.join(abs_dirpath, 'hooks.py')
-
+        abs_filepath = os.path.join(self.embryo_path, 'hooks.py')
         if os.path.isfile(abs_filepath):
-            print(abs_filepath)
-            sys.path.append(abs_dirpath)
             module = importlib.import_module('hooks')
             hook_manager = HookManager(
                 pre_create=getattr(module, 'pre_create', None),
                 post_create=getattr(module, 'post_create', None), )
-            sys.path.pop()
         else:
             hook_manager = HookManager()
 

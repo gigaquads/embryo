c9c351c1c3a3360e76543458ae51f0d5ea47af55
Jeff Doss <notdsk@gmail.com>
2018-01-07 03:24:59 -0500

    react-project: Base react project
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

---
+++ embryo/embryos/react-project/templates/{{ name|dash }}/client/components/App.jsx
@@ -0,0 +1,13 @@
+{% raw -%}
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div style={{textAlign: 'center'}}>
+                <h1>Hello, world!</h1>
+            </div>
+        );
+    }
+}
+{%- endraw %}

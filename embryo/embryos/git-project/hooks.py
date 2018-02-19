889ff34ed6723a2f6bfb99942b2756e34639990e
Jeff Doss <notdsk@gmail.com>
2018-02-19 12:16:57 -0500

    inter-template-usage: Updating git project destination reference
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- embryo/embryos/git-project/hooks.py
+++ embryo/embryos/git-project/hooks.py
@@ -5,7 +5,7 @@ def pre_create(context):
     """
     Using the provided destination, initialize the git repository
     """
-    dest = context['args']['destination']
+    dest = context['dest']
     g = git.cmd.Git(dest)
     g.init()
     return True

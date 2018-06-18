f7700f705c57f2b668a28e2537fdfb0c4a8ec9db
Jeff Doss <notdsk@gmail.com>
2018-06-18 23:17:47 +0000

    condensed.embryo.context.schema: Condensed embryo context schema format
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- embryo/embryos/embryo/templates/{{ name|dash }}/embryo.py
+++ embryo/embryos/embryo/templates/{{ name|dash }}/embryo.py
@@ -4,20 +4,24 @@ from embryo.embryo import Embryo
 
 class {{ name|camel }}Embryo(Embryo):
     """
-    An embryo for {{ name|title }}
+    # {{name|title }} Embryo
     """
 
     class context_schema(Schema):
         """
+        # Context Schema
         The respective {{ name|title }} schema
+{% if schema_fields %}
+        
+        ## Fields
+{% for field in schema_fields %}
+        * `{{ field['name']|snake }}`: {{ field.get('description', 'TODO') }}
+{% endfor %}
+{% endif %}
         """
 {% if schema_fields %}
 {% for field in schema_fields %}
-
-        {{ field['name'] }} = fields.{{ field['type'] }}()
-        """
-        {{ field['name']|title }}
-        """
+        {{ field['name']|snake }} = fields.{{ field['type']|camel }}()
 {% endfor %}
 {% else %}
         pass

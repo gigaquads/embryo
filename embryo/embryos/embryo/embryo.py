6a429437945d5d4df4d1f620972b3c8d67b2f520
Jeff Doss <notdsk@gmail.com>
2018-07-11 19:03:13 -0400

    git-embryo-upgrade: Misc
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- embryo/embryos/embryo/embryo.py
+++ embryo/embryos/embryo/embryo.py
@@ -16,4 +16,18 @@ class EmbryoEmbryo(Embryo):
         - `schema_fields` Schema fields to be applied to an embryo
         """
         name = fields.Str()
-        schema_fields = fields.List(nested=fields.Dict())
+        schema_fields = fields.List(nested=fields.Dict(), default=[])
+
+    def pre_create(self):
+        """
+        The most complete schema fields is a dictionary of data, including name
+        , description, and even type.  However, sometimes you want to simply
+        and quickly define a few fields without being specific as to what they
+        are.  All of this is of course optional.
+
+        If a string is provided then it will be cast into basic schema_fields
+        with name only.
+        """
+        schema_fields = self.context.get('schema_fields', [])
+        if isinstance(schema_fields, str):
+            self.context['schema_fields'] = [dict(name=schema_fields)]
